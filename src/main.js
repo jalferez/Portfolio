29026b35c9952e2dd30307051fcc24950050f955
jalferez <j.alferez@swau.edu>
2024-02-04 18:11:49 -0600

    Set Base url of page

--- src/main.js
+++ src/main.js
@@ -3,3 +3,11 @@ import '../styles/style.css'
 import '../styles/components/header.css'
 import '../styles/components/hero.css'
 import '../styles/utils.css'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    base: '/deploying-vite-project-Portfolio/',
+    plugins: [vue()]
+    })
