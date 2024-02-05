8dbbb6fca73e82100166ee57e52c1f48ec036b4b
jalferez <j.alferez@swau.edu>
2024-02-04 18:13:40 -0600

    Reverted Changes for GitHub Pages

--- src/main.js
+++ src/main.js
@@ -4,10 +4,3 @@ import '../styles/components/header.css'
 import '../styles/components/hero.css'
 import '../styles/utils.css'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-    base: '/deploying-vite-project-Portfolio/',
-    plugins: [vue()]
-    })
